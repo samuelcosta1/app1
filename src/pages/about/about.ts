9ebd10f9d2ba26fd28b6fecba707d24cf0905eeb
unknown <totokillergamer@gmail.com>
2018-04-19 13:17:45 -0300

    first commit

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -9,6 +9,6 @@ export class AboutPage {
 
   constructor(public navCtrl: NavController) {
 
+  
   }
-
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  constructor(public alertCtrl: AlertController) {
   }
-
 }
